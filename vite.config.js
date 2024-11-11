b574d58038ac4904e34418b4e0a961be747e22c7
admin <adminibl@sucafina.com>
2024-11-11 09:33:15 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,44 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   base:"/",
+// })
+
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  },
+  base: '/', // This is important for routing
+  server: {
+    host: true,
+  },
+  preview: {
+    port: 8011,
+    strictPort: true,
+    host: true,
+  }
 })
